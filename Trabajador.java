1446ec07208905c219ea624a5ee18d207a74af99
RoniPG <153464839+RoniPG@users.noreply.github.com>
2023-12-12 16:25:51 +0100

    Add files via upload

---
+++ Trabajador.java
@@ -0,0 +1,19 @@
+
+public class Trabajador extends Usuario {
+	
+	private int codigo_trabajador;
+
+	public Trabajador(String nombre, String apellido, String user, String password, String email, int edad, int codigo_trabajador) {
+		super(nombre, apellido, user, password, email, edad);
+		this.codigo_trabajador=codigo_trabajador;
+	}
+
+	public int getCodigo_trabajador() {
+		return codigo_trabajador;
+	}
+
+	public void setCodigo_trabajador(int codigo_trabajador) {
+		this.codigo_trabajador = codigo_trabajador;
+	}
+	
+}
