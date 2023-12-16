78167691c7bfa01e771a76fe9a50f75c0a528f53
RoniPG <ronigmn@gmail.com>
2023-12-16 16:27:13 +0100

    Añadimos el constructor de la clase

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,8 @@
 public class Cliente extends Usuario{
 	
+    public Cliente(String nombre, String apellido, String user, String password, String email, int edad) {
+        super(nombre, apellido, user, password, email, edad);
+    }
+	
 }
 
