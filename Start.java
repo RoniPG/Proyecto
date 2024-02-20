2dd3c2edc1582cd5a266a2af605a914ef09fd48b
RoniPG <ronigmn@gmail.com>
2024-02-20 14:51:24 +0100

    "Código para ejecutar la interfaz gráfica"

--- Start.java
+++ Start.java
@@ -4,6 +4,10 @@
 public class Start {
  
     public static void main(String[] args) {
+        App login = new App();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);        
+        
         ConectarDB conexion = new ConectarDB();
       //conexion.verTablas(); ---> Llamada al metodo oculto
         conexion.desconectar();
